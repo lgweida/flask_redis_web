4bb224bd06458255b8ab93b34b4dbb45e27bf5c9
Li Gu <ligu2006@gmail.com>
2022-10-30 14:52:52 -0400

    diable redis

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import quickfix as qf
 import json
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+#cache = redis.Redis(host='redis', port=6379)
 
 
 def get_hit_count():
@@ -22,7 +22,7 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
+#    count = get_hit_count()
     print(qf)
     #return 'Hello World! I have been seen {} times. {}\n'.format(count, json.dumps(dir(qf)))
     return json.dumps(dir(qf))
