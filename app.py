545ce49ffd8c92c64e19d056079e5a930ce69345
lgweida <45736060+lgweida@users.noreply.github.com>
2023-05-16 16:44:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ stock = finvizfinance('tsla')
 quote = Quote()
 q = quote.get_current("tsla");
 
+
 app = Flask(__name__)
 #cache = redis.Redis(host='redis', port=6379)
 
@@ -30,6 +31,6 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times. {}\n'.format(count, "hello World")
+    return 'Hello World! I have been seen {} times. {}\n'.format(count, q)
     
    
